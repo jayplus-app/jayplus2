f4ab5b874adfab275bc6810ff9f9a1e162c47b05
Ben Cyrus <62337982+bencyrus@users.noreply.github.com>
2023-08-23 12:30:21 +0000

    added basic App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+
 function App() {
+	const [message, setMessage] = useState<string | null>(null)
+
+	useEffect(() => {
+		fetch('http://localhost:8080/hello')
+			.then((response) => response.text())
+			.then((data) => setMessage(data))
+			.catch((error) => console.error('Error fetching data: ', error))
+	}, [])
+
 	return (
 		<div className="App">
-			<h1>Hello World</h1>
+			<header className="App-header">
+				{message ? <p>{message}</p> : <p>Loading...</p>}
+			</header>
 		</div>
 	)
 }
