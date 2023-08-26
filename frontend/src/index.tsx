5dbacaa26dfaa2a0a9ddbaa2092c1ad194d3e769
Ben Cyrus <62337982+bencyrus@users.noreply.github.com>
2023-08-26 19:48:29 +0000

    fixed router issue

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,6 +6,6 @@ import router from './routes'
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
 	<React.StrictMode>
-		<RouterProvider router={router}>
+		<RouterProvider router={router} />
 	</React.StrictMode>
 )
