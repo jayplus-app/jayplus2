234bde04408bcc2a780cb9e6378f1d794f299d58
Ben Cyrus <62337982+bencyrus@users.noreply.github.com>
2023-09-04 05:44:15 +0000

    clean ups

--- backend/internal/app/app.go
+++ backend/internal/app/app.go
@@ -12,16 +12,16 @@ import (
 )
 
 type App struct {
-	DB     		db.DBInterface
-	Router 		*mux.Router
-	Auth   		auth.AuthInterface
-	Booking 	booking.BookingInterface
+	DB      db.DBInterface
+	Router  *mux.Router
+	Auth    auth.AuthInterface
+	Booking booking.BookingInterface
 }
 
 func NewApp(dbInstance db.DBInterface, authInstance auth.AuthInterface, bookingInstance booking.BookingInterface) (*App, error) {
 	app := &App{
-		DB:   dbInstance,
-		Auth: authInstance,
+		DB:      dbInstance,
+		Auth:    authInstance,
 		Booking: bookingInstance,
 	}
 	app.Router = app.SetupRouter()
