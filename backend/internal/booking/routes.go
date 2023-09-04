f2045352e8b5f8a31a89f175da90a9cc78598ef6
Ben Cyrus <62337982+bencyrus@users.noreply.github.com>
2023-09-04 04:56:32 +0000

    clean ups

--- backend/internal/booking/routes.go
+++ backend/internal/booking/routes.go
@@ -18,7 +18,7 @@ func BookingRoutes(r *mux.Router, booking booking.BookingInterface, db booking.B
 		}
 		return
 	}
-	
+
 	bookingRouter := r.PathPrefix("/booking").Subrouter()
 
 	bookingRouter.HandleFunc("/vehicle-types", func(w http.ResponseWriter, r *http.Request) {
